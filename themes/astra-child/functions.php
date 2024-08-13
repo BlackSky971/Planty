1e2cbe6a61c9c69a0b44a8ab45c1ec90705fc88c
Yoan Mamadou <blacksky971@MacBook-Pro-de-Yoan.local>
2024-08-13 09:41:54 -0400

    Modifications photos et css Elementor

--- themes/astra-child/functions.php
+++ themes/astra-child/functions.php
@@ -22,3 +22,28 @@ endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+// Hook to add a custom menu item
+add_action('wp_nav_menu_items', 'add_admin_menu_link', 10, 2);
+
+function add_admin_menu_link($items, $args) {
+    // Check if the user is logged in
+    if (is_user_logged_in() && $args->menu === 'menu') {
+        // Add the admin link
+        $admin_link = '<li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item current_page_item parent hfe-creative-menu"><a class="hfe-menu-item" href="' . admin_url() . '">Admin</a></li>';
+        $items .= $admin_link;
+    }
+    return $items;
+}
+// Optional: Add a custom menu location
+add_action('init', 'register_custom_menu_location');
+function register_custom_menu_location() {
+    register_nav_menu('primary', __('Primary Menu'));
+}
+// responsive menu
+function set_container_class($args)
+{
+	$args['container_class'] = str_replace(' ', '-', $args['theme_location']) . '-nav';
+	return $args;
+}
+add_filter('wp_nav_menu_args', 'set_container_class');
